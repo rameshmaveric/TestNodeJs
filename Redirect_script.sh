48f1d88d672af1c19b77d0a70c7c7c332198ca69
rameshmaveric <rameshuser123@gmail.com>
2017-09-19 13:12:07 +0530

    Update Redirect_script.sh
    test

--- Redirect_script.sh
+++ Redirect_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-    curl -H 'Authorization: 000eb7f654e311fdb32241e988301c99dce8a471' --resolve https://github.com:443:192.30.253.112 -X POST https://github.com/rameshmaveric/webappDepolyment.git:443/ -F filedata=@file.txt
+    curl -H 'Authorization: 000eb7f654e311fdb32241e988301c99dce8a471' --resolve https://github.com:443:192.30.253.112 -X POST https://github.com/rameshmaveric/webappDepolyment.git:443/ -F filedata=@display_script
